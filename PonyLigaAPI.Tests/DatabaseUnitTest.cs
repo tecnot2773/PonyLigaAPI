cc69f740fb713dcbb6a6c362450bfb8668c63de3
Timon <timon.borchers@googlemail.com>
2021-06-08 14:28:52 +0200

    Unit test for ConnectionString

---
+++ PonyLigaAPI.Tests/DatabaseUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PonyLigaAPI.Tests
+{
+    public class DatabaseUnitTest
+    {
+        [Fact]
+        public void TestBuildConnectionString()
+        {
+            var envString = Environment.GetEnvironmentVariable("MYSQLCONNSTR_localdb");
+            if (envString != null)
+            {
+                var connectionString = Models.PonyLigaAPIContext.BuildConnectionString();
+
+                Assert.Matches("server=.*;port=.*;database=pony_liga;user=.*;password=.*", connectionString);
+            }
+            
+        }
+    }
+}
